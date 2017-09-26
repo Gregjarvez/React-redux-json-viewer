6ca7c14b63d43e1693b3d3384da3d39ec4f16ab1
Gregory Assasie <gregjarvez@gmail.com>
2017-09-26 02:27:58 +0100

    delimiter bug fixed

--- app/parser/pathFinder.js
+++ app/parser/pathFinder.js
@@ -25,7 +25,7 @@ const PathFinder = (function () {
   function composePath(pathObject) {
     const steps = reducePath(pathObject);
     const delimiters = steps.map(node => node.type);
-    // delimiters.pop(); // removes target delimiter
+    delimiters.pop(); // removes target delimiter
     const template = steps.reduce((cur, prev, index) => {
       const currentDelimiter = delimiters[((index - 1) + 1)];
       return cur
