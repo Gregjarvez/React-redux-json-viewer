33ef09bcad1727e5538d22be4d22feaf35962b16
Gregory Assasie <gregjarvez@gmail.com>
2017-12-20 16:40:56 +0000

    remove load url function

--- app/app.js
+++ app/app.js
@@ -8,26 +8,7 @@ import Modal from './components/url_modal';
 import { fetchRequestedUrl, validUrl } from './parser/demo';
 
 class App extends Component {
-
-  state = {
-    errorMessage: '',
-    urlModalRequest: false,
-    urlErrorMessage: ''
-  };
-
-  loadUrl = (url) => {
-    if (validUrl(url)) {
-      fetchRequestedUrl(url).then((json) => {
-        this.setState({
-          json: JSON.stringify(json, null, this.state.tabSize),
-          urlModalRequest: false,
-          urlErrorMessage: ''
-        });
-        this.setTree();
-      }).catch(err => this.setState({ urlErrorMessage: err.message }));
-    }
-  };
-
+  
   render() {
     return (
       <div className="container">
