8c1bb99ba1ff572178e7e013d1c7b47a31a58d3a
Gregory Assasie <gregjarvez@gmail.com>
2017-12-20 17:21:53 +0000

    Update middleware.js

--- app/redux/middleware.js
+++ app/redux/middleware.js
@@ -4,7 +4,13 @@ import constants from './constants';
 export const inspectJson = store => next => (action) => {
   if (action.type === constants.PARSE_FIRST_LAYER) {
     const status = checkJsonValidity(store.getState().json);
-    status.error && next({type: constants.PARSE_FAILED});
+    status.error && next({
+      type: constants.PARSE_FAILED,
+      payload: {		
+         error: maybeError.error,		
+         errorMessage: maybeError.errorMessage		
+      }
+    });
   }
   return next(action);
 };
